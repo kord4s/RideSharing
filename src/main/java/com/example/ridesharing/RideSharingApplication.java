80abf788454dc2effed8de55478be135c2128a46
Patryk Kordasiński <UL0255057@edu.uni.lodz.pl>
2022-12-16 17:43:57 +0100

    Initial commit

---
+++ src/main/java/com/example/ridesharing/RideSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ridesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ridesharing/Journey/JourneyPickUp/Journey.java
@@ -0,0 +1,2 @@
+package com.example.ridesharing.Journey.JourneyPickUp;public class Journey {
+}
